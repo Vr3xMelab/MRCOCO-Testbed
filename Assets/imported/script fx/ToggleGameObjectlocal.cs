ff8cadc1b083f1e63d783d333e9a4fd561e8b2a9
fabixdev <123942922+fabixdev@users.noreply.github.com>
2024-12-09 12:57:02 +0100

    new ui_

--- Assets/imported/script fx/ToggleGameObjectlocal.cs
+++ Assets/imported/script fx/ToggleGameObjectlocal.cs
@@ -5,12 +5,14 @@ public class ToggleGameObjectlocal : MonoBehaviour
     // Metodo per accendere il GameObject
     public void TurnOn()
     {
+        UnityEngine.Debug.Log($"{gameObject.name}: TurnOn called");
         gameObject.SetActive(true);
     }
 
     // Metodo per spegnere il GameObject
     public void TurnOff()
     {
+        UnityEngine.Debug.Log($"{gameObject.name}: TurnOff called");
         gameObject.SetActive(false);
     }
 
@@ -19,10 +21,12 @@ public class ToggleGameObjectlocal : MonoBehaviour
     {
         if (gameObject.activeSelf)
         {
+            UnityEngine.Debug.Log($"{gameObject.name}: GameObject is active, turning off");
             TurnOff();
         }
         else
         {
+            UnityEngine.Debug.Log($"{gameObject.name}: GameObject is inactive, turning on");
             TurnOn();
         }
     }
